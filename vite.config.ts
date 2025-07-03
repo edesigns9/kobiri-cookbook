cd19b99a27e8c31b376fa6a1011e73f82dba24fd
Your Name <your-email@example.com>
2025-07-03 04:17:34 +0200

    updated port method

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        minimal: path.resolve(__dirname, 'minimal.html'),
       },
     },
   },
